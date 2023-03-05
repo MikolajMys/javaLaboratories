847fae03b44e28acb1f17603e0d8a25d706027d5
miki <mikolaj.my@gmail.com>
2023-03-05 21:34:44 +0100

    zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-/*Zadanie 1.
-Zdefiniuj klasę Point posiadającą dwa publiczne, ostateczne pola x, y.
-Napisz konstruktor ustawiający te wartości.
-Zdefiniuj klasę Segment reprezentującą odcinek, posiadającą dwa prywatne punkty klasy Point.
-Wygeneruj akcesory i mutatory klasy Segment. Napisz publiczną metodę, która zwraca długość odcinka.
-W kolejnym kroku usuń mutatory i utwórz konstruktor ustawiający
-te pola na wartości swoich dwóch parametrów.*/
+/*Zadanie 2.
+Zdefiniuj w klasie Segment publiczną metodę toSvg(),
+która zwróci napis zawierający kod języka znacznikowego SVG pozwalający wyświetlić tę linię.*/
 public class Main {
     public static void main(String[] args) {
         //comment
@@ -14,6 +10,6 @@ public class Main {
         Segment segment=new Segment(p1, p2);
         double distance=segment.getDistance();
 
-        System.out.println(distance);
+        System.out.println(segment.getSvg());
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,7 +1,9 @@
+import java.util.Locale;
 public class Segment {
     private Point a;
     private Point b;
-
+    //" <line x1="20" y1="20" x2="80" y2="80" style="stroke:black" />"
+    //    String test = String.format(Locale.ENGLISH, " %f ", 2);
     public Segment(Point a, Point b){
         this.a=a;
         this.b=b;
@@ -22,5 +24,9 @@ public class Segment {
         double dx = b.x - a.x;
         double dy = b.y - a.y;
         return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+    public String getSvg(){
+        String result = String.format(Locale.ENGLISH,"<line x1=\"%.2f\" y1=\"%.2f\" x2=\"%.2f\" y2=\"%.2f\" style=\"stroke:black\" />", a.x, a.y, b.x, b.y);
+        return result;
     }
 }
